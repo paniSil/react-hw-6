95e6bcdcd20fce40067b1f3f6e7852f5d966562d
paniSil <mzwonko@gmail.com>
2025-04-06 16:40:52 +0300

    upd

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
 const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <p>Page about our team</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Page about our team</p>
+    </div>
+  )
 }
 
 export default About
